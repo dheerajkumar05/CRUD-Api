130ae1c9e9cef7641521299739c26849baa739f0
“dheeraj” <dheeraj@example.com>
2024-03-05 11:28:06 +0530

    add admin Rspecc

--- spec/admin/admin_user_spec.rb
+++ spec/admin/admin_user_spec.rb
@@ -14,22 +14,22 @@ RSpec.describe "AdminUsers", type: :feature do
       fill_in "admin_user[password]", with: "password"
       fill_in "admin_user[password_confirmation]", with: "password"
       click_button "Create Admin user"
-      # expect(page).to have_content("Admin user was successfully created.")
+       expect(page).to have_content("Admin user was successfully created.")
     end
 
     it "edits an admin user" do
       visit edit_admin_admin_user_path(admin_user)
       fill_in "Email", with: "updated_admin@example.com"
       click_button "Update Admin user"
-      # expect(page).to have_content("Admin user was successfully updated.")
-      # expect(page).to have_content("updated_admin@example.com")
+       expect(page).to have_content("Admin user was successfully updated.")
+       expect(page).to have_content("updated_admin@example.com")
     end
 
     it "deletes an admin user" do
       visit admin_admin_users_path
       click_link "Delete", match: :first
-      # expect(page).to have_content("Admin user was successfully destroyed.")
-      #\ expect(page).not_to have_content(admin_user.email)
+      expect(page).to have_content("Admin user was successfully destroyed.")
+      expect(page).not_to have_content(admin_user.email)
     end
   end
 end
